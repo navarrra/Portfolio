c6708d41291648acc11ce4d0fa7c0a31c891f06e
Ryan Navarro <ryannavarro@ymail.com>
2017-12-29 18:06:25 -0800

    fixed backgrounds to small file sizes

--- scripts.js
+++ scripts.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
   });
 
 
-
+  //smooth scroll
   $("a").click(function(e){
     var value = this.hash;
     //prevent anchor from going directly to the page
       e.preventDefault();
     $("body, html").animate({
         scrollTop: $(value).offset().top
-    });
+    }, 1000);
   });
 
 
